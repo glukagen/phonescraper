012d6bfe7b5cd90c265c036c26b4a91cf209f35b
Max Yurlov <maxyurloff@gmail.com>
2011-04-25 00:00:31 +0300

    create web apllication using django

---
+++ addressbook/message/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from models import *
+
+class MessageAdmin(admin.ModelAdmin):
+    model = Message
+    list_display = ('sender', 'recipient', 'sender_name', 'sender_email', 'subject', 'payload', 'date',)
+    
+
+class PhoneNumberAdmin(admin.ModelAdmin):
+    model = PhoneNumber
+    list_display = ('name', 'email', 'value')
+    
+    search_fields = ['message__sender_name', 'message__sender_email', 'value',]
+    
+    def name(self, phoneNumber):
+        return phoneNumber.message.sender_name
+    
+    def email(self, phoneNumber):
+        return phoneNumber.message.sender_email
+
+
+admin.site.register(Message, MessageAdmin)
+admin.site.register(PhoneNumber, PhoneNumberAdmin)
---
+++ addressbook/message/management/commands/spyder.py
@@ -0,0 +1,36 @@
+
+from django.core.management.base import BaseCommand, CommandError
+import datetime
+from BeautifulSoup import BeautifulSoup
+from rotator.models import Account
+from rotator.spyderhandler import *
+
+from rotator.models import Network
+from rotator import spyder_objects
+from string import capitalize
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):   
+        now = datetime.datetime.now()   
+        print now 
+        networks = {            
+            "http://getads.com/": GetAdsHandler, 
+            "http://affiliate.com/": AffiliateComHandler, 
+            "https://network.hydranetwork.com/login": HydraHandler,
+            'http://acpaffiliates.com/Publishers': ACPAffiliatesHandler,
+            'http://www.c2mtrax.com/': APIHandler,
+            'http://www.ecoretrax.com/': APIHandler,
+            #'http://www.ads4dough.com/': Ads4DoughHandler,
+            'https://adscendmedia.com/': AdscendHandler,
+            'http://www.epicdirectnetwork.com/': AzoogleHandler,
+            'http://emt.copeac.com/forms/login.aspx': CopeacHandler,
+        }
+        #networks = { "http://getads.com/": GetAdsHandler}
+        
+        for account in Account.objects.all():
+            if account.network.url in networks:                
+                if(networks[account.network.url](now, account).run()):
+                    account.checked()
+  
+        print "Finished."
